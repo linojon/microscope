be915d1589e8b1a51870b781cb3ecc9941a32bc9
Jonathan Linowes <jonathan@parkerhill.com>
2014-08-20 14:07:23 -0400

    edit post

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,59 @@
 Posts = new Meteor.Collection('posts');
+
+Posts.allow({
+  // insert: function(userId, doc) {
+  //   // only allow posting if you are logged in
+  //   return !! userId;
+  // }
+ 
+  update: ownsDocument,
+  remove: ownsDocument
+});
+
+Posts.deny({
+  update: function(userId, post, fieldNames) {
+    // may only edit the following fields
+    return (_.without(fieldNames, 'url', 'title').length > 0);
+  }
+})
+
+Meteor.methods({
+  post: function(postAttributes) {
+    var user = Meteor.user(),
+        postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+    // ensure user is logged in
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to post new stories");
+
+    // ensure the post has a title
+    if (!postAttributes.title)
+      throw new Meteor.Error(422, "Please fill in a headline");
+
+    // check link is unique
+    if (postAttributes.url && postWithSameLink) 
+      throw new Meteor.Error(302, "This link has already been posted", postWithSameLink._id);
+    
+    // pick out the whitelisted keys
+    var post = _.extend(_.pick(postAttributes, 'url', 'message'), {
+      title: postAttributes.title + (this.isSimulation ? '(client)' : '(server)'),
+      userId: user._id,
+      author: user.username,
+      submitted: new Date().getTime()
+    });
+
+    // wait 5 seconds
+    if (! this.isSimulation) {
+      var Future = Npm.require('fibers/future');
+      var future = new Future();
+      Meteor.setTimeout(function() {
+        future.return();
+      }, 5 * 1000);
+      future.wait();
+    }
+
+    var postId = Posts.insert(post);
+
+    return postId;
+  }
+});
